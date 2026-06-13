d1e3e7ebe4b319903163e31b1f10d9e1fc798d32
zayennn <zaayeennn@gmail.com>
2026-06-13 13:58:05 +0700

    update

---
+++ belajar dulu/src/MainForm.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class MainForm {
+    private JPanel main_form;
+    private JLabel nim;
+    private JLabel nama;
+    private JLabel fakultas;
+    private JTextField input_nim;
+    private JTextField input_nama;
+    private JTextField input_fakultas;
+    private JTable data_table;
+}
